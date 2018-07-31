74eb8a18528de35b6f9856ac297cf65aa012ebe7
administrator <administrator@jarvis.local>
2018-07-31 13:22:13 +0530

    push notification

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser=require('body-parser')
 const path=require('path')
 const db=require('./db_connections/mongodb')
 const app=express();
+var FCM = require('fcm-push');
 const cors = require('cors');
 const TermsAndPolicy=("../model/termsAndPrivacyModel");
 var Twocheckout = require('2checkout-node');
@@ -34,5 +35,46 @@ app.use('/api/v1/terms',require('./routes/terms'))
 app.use('/api/v1/organizer',require('./routes/organizer'));
 app.use('/api/v1/player',require('./routes/player.js'));
 app.use('/api/v1/data',require('./routes/data.js'));
+
+
+
+
+
+
+
+
+var serverKey = 'AAAAhTQQvgs:APA91bHYy14WxdIN6yAmGZJ3Utu07vukU3Yjonevmgq5-_zUeTkmgo2Aw_ldeNHJ7vLC29Iquoc1IqBQqabD_qSLDypMY3axWuHoN0N7JYMj3EbNyzmg9ApYQYDBiDiI9SZnt1JRVoWoYjV5ZVFZEOmGzMedL1Vv5A';
+var fcm = new FCM(serverKey);
+
+var message = {
+    to: 'ddMQdHYWfB4:APA91bHmiaJtIJAlonDRDEKSlZFi3-6tvvMJ9qRIs_IBRbZakJG1HUgmOZRkHQJ54uVwvcuPXhGHk-cc3AmZL0Cvnnklx5wC7-nQQXQtAiB5D5ttAOR-RkBZI6ZrjLeOD9uh6SttStoN2g2dmETfBpRqTpqUUhtXqQ', // required fill with device token or topics
+    // collapse_key: 'your_collapse_key', 
+    // data: {
+    //     your_custom_data_key: 'your_custom_data_value'
+    // },
+    notification: {
+        title: 'YALA App',
+        body: 'Player is added !'
+    }
+};
+
+//callback style
+fcm.send(message, function(err, response){
+    if (err) {
+        console.log("Something has gone wrong!");
+    } else {
+        console.log("Successfully sent with response: ", response);
+    }
+});
+
+// //promise style
+// fcm.send(message)
+//     .then(function(response){
+//         console.log("Successfully sent with response: ", response);
+//     })
+//     .catch(function(err){
+//         console.log("Something has gone wrong!");
+//         console.error(err);
+//     })
 
 app.listen(app.get('port'),()=>console.log('Server running on ' + app.get('port')));
